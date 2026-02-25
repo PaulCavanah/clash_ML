54d072ed404f88e00ff0e90dd271374c958e3155
Paul Cavanah <cavanahpaul@gmail.com>
2026-02-25 12:00:55 -0500

    Wrote CSV_to_parquet.py (not finished) Added parquet_to_deck_onehot

---
+++ parquet_to_deck_onehot.py
@@ -0,0 +1,4 @@
+# For model training, there is X and Y data required 
+# This function takes the relevant columns from the parquet file and 
+# In the parquet files, there are 16 columns which hold the deck information
+# 
