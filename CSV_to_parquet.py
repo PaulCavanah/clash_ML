acc13020db5389ec470bb09f84a29d79c262b5e4
Paul Cavanah <cavanahpaul@gmail.com>
2026-02-26 08:06:00 -0500

    Progress on parquet_to_deck_onehot.py

---
+++ CSV_to_parquet.py
@@ -0,0 +1,82 @@
+# Script to batch-create parquet files from raw csv data
+
+#%%
+from pathlib import Path
+import shutil 
+import pandas as pd 
+import os
+import glob
+import sys
+import datetime
+
+#%%
+# Define paths
+raw_dir = Path(os.getcwd() + "/data/raw_data")
+done_dir = Path(os.getcwd() + "/data/raw_data_done")
+parquet_dir = Path(os.getcwd() + "/data/parquet")
+
+raw_dir.mkdir(parents = True, exist_ok = True)
+parquet_dir.mkdir(parents = True, exist_ok = True)
+
+#%% 
+# Define data types for each column
+d_types = {
+    "player_tag" : "object",
+    "player_trophies" : "uint16",
+    "player_crowns" : "uint8",
+    "opponent_tag" : "object",
+    "opponent_trophies" : "uint16",
+    "opponent_crowns" : "uint8",
+    "gamemode" : "object", 
+    "game_time" : "object", 
+    "p_king_health" : "uint16",
+    "p_support_1_health" : "uint16",
+    "p_support_2_health" : "uint16",
+    "p_support_level" : "uint8",
+    "o_king_health" : "uint16",
+    "o_support_1_health" : "uint16",
+    "o_support_2_health" : "uint16",
+    "o_support_level" : "uint8",
+    "p_tower" : "uint32",
+    "o_tower" : "uint32"
+} | \
+{f"p_card_{i+1}" : "uint32" for i in range(8)} | \
+{f"p_card_{i+1}_level" : "uint8" for i in range(8)} | \
+{f"p_card_{i+1}_evohero" : "uint8" for i in range(8)} | \
+{f"o_card_{i+1}" : "uint32" for i in range(8)} | \
+{f"o_card_{i+1}_level" : "uint8" for i in range(8)} | \
+{f"o_card_{i+1}_evohero" : "uint8" for i in range(8)} 
+
+# sys.getsizeof(retyped_df) is ~0.5 sys.getsizeof(raw_df)
+
+#%%
+# Get filenames of raw data and establish batch size
+raw_data_names = [filepath.name for filepath in raw_dir.glob("*.csv")]
+num_files = len(raw_data_names)
+csv_batch_size = 100 # number of csv files to convert to parquet at a time
+num_batches = num_files // csv_batch_size
+
+#for batch in range(num_batches) : 
+
+df_list = []
+for file_ii in range(csv_batch_size) : 
+    single_df = pd.read_csv(raw_dir / raw_data_names[file_ii])
+    single_df.fillna(0, inplace = True) #replace NaN values with 0 
+    for col, dt in d_types.items() : 
+        single_df[col] = single_df[col].astype(dt)
+    df_list.append(single_df)
+df = pd.concat(df_list, ignore_index = True) #Contains the re-typed data from all of the batch CSVs
+
+# Use timestamp to uniquely identify the parquet batch file
+dt = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+batch_filepath_tmp = parquet_dir / (dt + ".parquet.tmp")
+batch_filepath = parquet_dir / (dt + ".parquet")
+df.to_parquet(batch_filepath_tmp, engine = "pyarrow", compression = "zstd", index = False)
+# After saving successfully, convert .tmp to .parquet (.tmp makes crashing during saves not a problem)
+batch_filepath_tmp.replace(batch_filepath)
+
+# Move the 
+
+
+
+# %%
--- parquet_to_deck_onehot.py
+++ parquet_to_deck_onehot.py
@@ -1,4 +1,57 @@
 # For model training, there is X and Y data required 
-# This function takes the relevant columns from the parquet file and 
-# In the parquet files, there are 16 columns which hold the deck information
-# 
+# In this case, X is a one-hot matrix of all cards, including evo/hero versions
+# This function converts the relevant columns from the parquet file to X
+
+#%%
+from pathlib import Path
+import pandas as pd 
+import os 
+import sys
+import requests
+import json
+
+#Apt
+#TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjNiZjBiMzZiLTVlMzQtNGFhZi05MTNkLWUwZmJhMzFiYjJkYyIsImlhdCI6MTc2OTMxNjAwOSwic3ViIjoiZGV2ZWxvcGVyLzhjNzUyNDE0LWIzNzItYjc4Ny0zOTk0LTZlMGExZWEzN2RmZCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3Ni4zNy4yNDkuMTU2Il0sInR5cGUiOiJjbGllbnQifV19.c_Z8GSEGA6i8iwWTV6ZUrOVZAVTkGn6vmzPafa6GJRqEMEgyQSlEjzSJnkuPz689Udd2JZUAih0FbgzcmAGRhg"
+
+#K apt 
+#TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjY4MmNjOGNmLTcxMzktNDNmOS1hZmM5LWM1NzgyYjAwMGJlNSIsImlhdCI6MTc3MTAyNjgwMywic3ViIjoiZGV2ZWxvcGVyLzhjNzUyNDE0LWIzNzItYjc4Ny0zOTk0LTZlMGExZWEzN2RmZCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3Ni4zNy4yNTAuMTgiXSwidHlwZSI6ImNsaWVudCJ9XX0.8-Fwy4Unu3TQFOn4opxt_A9r2bOxu1VOOUY6jvqV1QcciJdjNCjK6IOPVm0T7vKvqvWYjfR7GhnYJoGtSri3fQ"
+
+#Hospital
+TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjBiODkwMjBkLTY4YWMtNDhkYS05NTcwLTEyZTllNzFhM2NlOCIsImlhdCI6MTc2OTM2NjQwNCwic3ViIjoiZGV2ZWxvcGVyLzhjNzUyNDE0LWIzNzItYjc4Ny0zOTk0LTZlMGExZWEzN2RmZCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxMjguMTUxLjcxLjIzIl0sInR5cGUiOiJjbGllbnQifV19._7in6Y-a-cSSGzzXBjMZB0HZaRO-3qIjxCs9cphtEJ0weFyuCXinSyxqcgtgP7LFrCQMJc4dIC1ncAJXq1ynXA"
+
+parquet_dir = Path(os.getcwd() + "/data/parquet")
+
+# Data = card information (id and evo/hero status) for each game
+data_columns = [f"p_card_{i+1}" for i in range(8)] \
++ [f"o_card_{i+1}" for i in range(8)] \
++ [f"p_card_{i+1}_evohero" for i in range(8)] \
++ [f"o_card_{i+1}_evohero" for i in range(8)] 
+
+# %%
+
+# Read only data required for X 
+df = pd.read_parquet(path = parquet_dir, engine = "pyarrow", columns = data_columns)
+#print(df.dtypes, sys.getsizeof(df))
+
+# %%
+
+# Load card names from cards json or API call (the same thing, but cards json can be used for debugging/testing in certain situations where API can't)
+card_get_method = "JSON" # "JSON" or "API"
+if card_get_method == "API" : 
+    url = f"https://api.clashroyale.com/v1/cards"
+    headers = {"Authorization": f"Bearer {TOKEN}"}
+    r = requests.get(url, headers = headers)
+    card_data = r.json()
+elif card_get_method == "JSON": 
+    json_path = os.getcwd() + "/json/" + "Cards.json"
+    with open(json_path, 'r') as file : 
+        card_data = json.load(file) 
+
+# %%
+cards = card_data["items"]
+# Three types of cards: default, evolution, and hero
+card_columns = card_data["items"]
+
+
+
+# %%
